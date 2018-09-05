2a035e19ef0cea009b8b42728bf1d36b075cd82f
Szymon S <fide.simon@gmail.com>
2018-09-05 21:56:48 +0200

    Initial commit - clean solution

---
+++ src/extensions/adminQuickBar/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IAdminQuickBarApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'AdminQuickBarApplicationCustomizerStrings' {
+  const strings: IAdminQuickBarApplicationCustomizerStrings;
+  export = strings;
+}
